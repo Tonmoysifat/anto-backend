e14905fe416290e64545ff5e3212eba85d551dca
Tonmuy Ahamed Sifat <98707041+Tonmoysifat@users.noreply.github.com>
2024-09-20 14:13:09 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const router = require("./routes/api");
 require("dotenv").config();
-
 const app = express();
-
-// Middleware
 app.use(cors());
-app.use(express.json()); // for parsing application/json
+app.use(express.json()); 
 
-// MongoDB connection
 const URL = process.env.DATABASE_URL;
 const OPTION = { user: "", pass: "", autoIndex: true };
 
@@ -19,10 +15,8 @@ mongoose
   .then(() => console.log("Database Connected"))
   .catch((err) => console.error("Database connection error:", err));
 
-// Routes
-app.use("/api", router); // Routes from your routes/api.js file
+app.use("/api", router);
 
-// Port configuration for Vercel
 const port = process.env.PORT || 5414; 
 app.listen(port, () => {
   console.log(`Backend is running on http://localhost:${port}`);
